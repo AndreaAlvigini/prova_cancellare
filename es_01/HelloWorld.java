f532b1297df0e0843620c70d5c6d4ae8000d231d
AndreaAlvigini <andreaalvigini@gmail.com>
2023-05-11 12:01:32 +0200

    es-01 vers 1.0

---
+++ es_01/HelloWorld.java
@@ -0,0 +1,20 @@
+/*
+Esercizio di test HelloWorld  
+Serve a testare l'area di lavoro e se è tutto configurato stampa "Hello, World!" nel terminale
+IMPORTANTE: è possibile formattare il documento attraverso tasto destro "formatta documento", 
+allinea le parentesi grafe dovrebbero essere allineate se raggruppano lo stesso blocco di codice.
+A seconda della versione della jdk installata potrebbe essere necessario specificare l'estensione del file, 
+con il comando .java (esegue il file) come si fa con javac.
+per uscire dal terminale si può usare Ctrl c 
+*/
+
+import java.util.Scanner; // importa la classe Scanner dal package java.util
+
+public class HelloWorld { //RICORDARSI DI CAMBIARE NOME CLASSE
+
+  // il nome della classe deve essere uguale al nome del file Java (HelloWorld.java),
+
+  public static void main(String[] args) { // il metodo main è il punto di partenza dell'esecuzione del programma
+    System.out.println("Hello, world!"); // stampa "Hello, world!" nella console
+  }
+}
