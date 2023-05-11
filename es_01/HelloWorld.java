c7fa4473571d13cd75bd0c14a633775657a1f061
AndreaAlvigini <andreaalvigini@gmail.com>
2023-05-11 12:16:16 +0200

    HelloWorld vers 1.1

--- es_01/HelloWorld.java
+++ es_01/HelloWorld.java
@@ -15,6 +15,6 @@ public class HelloWorld { //RICORDARSI DI CAMBIARE NOME CLASSE
   // il nome della classe deve essere uguale al nome del file Java (HelloWorld.java),
 
   public static void main(String[] args) { // il metodo main è il punto di partenza dell'esecuzione del programma
-    System.out.println("Hello, world!"); // stampa "Hello, world!" nella console
+    System.out.println("bellaaaaaa"); // stampa "Hello, world!" nella console
   }
 }
